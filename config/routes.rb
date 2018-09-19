7425d0ab6753e71149e1adcf0b06798a46904d48
Yousef Kbeili <Yousef@users-air.gateway.sasktel.net>
2018-09-18 21:01:03 -0700

    create navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'welcome#index'
+  root 'welcome#index'
   get '/about_me', to: 'welcome#about'
   get('/contact_me',{to: 'contacts#index'})
   post('/contact_me', {to: 'contacts#create'})
