9cdd8b4e6bb58404f422c521b508163f717c8134
Yousef Kbeili <ykbeili@gmail.com>
2018-10-28 18:45:00 -0700

    Adding last name to the sender :D

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,9 +2,10 @@ class ContactsController < ApplicationController
   def index
   end
   def create
-    @name = params[:name]
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
     @email = params[:email]
     @text = params[:text]
-    ContactMeMailer.message_for_you(@name, @text).deliver_now
+    ContactMeMailer.message_for_you(@first_name,@last_name,@text).deliver_now
   end
 end
--- app/mailers/contact_me_mailer.rb
+++ app/mailers/contact_me_mailer.rb
@@ -1,7 +1,8 @@
 class ContactMeMailer < ApplicationMailer
-  def message_for_you(name, text)
-    @name = name
+  def message_for_you(first_name,last_name,text)
+    @first_name = first_name
+    @last_name = last_name
     @text = text
-    mail(to: 'ykbeili@gmail.com', subject: "#{@name} sent you a message")
+    mail(to: 'ykbeili@gmail.com', subject: "#{first_name} " " #{last_name} Sent you a message")
   end
 end
