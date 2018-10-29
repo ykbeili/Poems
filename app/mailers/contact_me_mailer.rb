daf5ae78d13d071a65012959eda5b7981da06f09
Yousef Kbeili <ykbeili@gmail.com>
2018-10-28 18:35:39 -0700

    Styling form and Navbar

--- app/mailers/contact_me_mailer.rb
+++ app/mailers/contact_me_mailer.rb
@@ -1,8 +1,7 @@
 class ContactMeMailer < ApplicationMailer
-
   def message_for_you(name, text)
     @name = name
     @text = text
-  mail(to: 'ykbeili@gmail.com', subject: "#{@name} sent you a message")
+    mail(to: 'ykbeili@gmail.com', subject: "#{@name} sent you a message")
   end
 end
