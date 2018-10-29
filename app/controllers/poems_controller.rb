2e748ad7b5a9c045ab3c37715c990a1c50bc0e08
Yousef Kbeili <ykbeili@gmail.com>
2018-10-28 19:32:00 -0700

    Adding New, Create and show poem

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,10 +3,15 @@ class PoemsController < ApplicationController
     @poem = Poem.new
   end
   def index
+    @poems = Poem.order(:created_at)
   end
   def create
-
+    poems_params = params.require(:poem).permit([:name, :description])
+    @poem = Poem.new poems_params
+    @poem.save
+    redirect_to root_path
   end
   def show
+    @poem = Poem.find params[:id]
   end
 end
